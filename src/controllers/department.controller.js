e843db17c3fbcd80539d59cc2b2cb8b9e0799871
ozkan <ozkandnz09@gmail.com>
2024-12-14 23:47:41 +0300

    department.controller

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -33,7 +33,8 @@ module.exports = {
   },
 
   update: async (req, res) => {
-    const data = await Department.updateOne({ _id: req.params.id }, req.body);
+    //! Does it perform update validaiton by default? No, with runValidators:true does it perform.
+    const data = await Department.updateOne({ _id: req.params.id }, req.body, {runValidators: true,});
     res.status(202).send({
       error: false,
       data,
@@ -42,10 +43,10 @@ module.exports = {
   },
 
   delete: async (req, res) => {
-    const data = await Department.deleteOne({_id:req.params.id})
+    const data = await Department.deleteOne({ _id: req.params.id });
     res.status(data.deletedCount ? 204 : 404).send({
-      error:!data.deletedCount,
+      error: !data.deletedCount,
       data,
-    })
+    });
   },
 };
