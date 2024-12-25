6c3e83985d8f83955f7408e2948e235d9ba2cb00
ozkan <ozkandnz09@gmail.com>
2024-12-25 23:36:17 +0300

    tokenmodel

---
+++ src/models/token.js
@@ -0,0 +1,28 @@
+"use Strict";
+
+const { mongoose } = require("../configs/dbConnection");
+
+// {
+//     "userId": "949813294910fdsa90d"
+//     "token": "...tokenKey..."
+// }
+
+const TokenSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Personnel",
+      required: true,
+      index: true,
+      unique: true,
+    },
+    token: {
+      type: String,
+      trim: true,
+      required: true,
+      index: true,
+      unique: true,
+    },
+  },
+  { collection: token, timestamps: true }
+);
