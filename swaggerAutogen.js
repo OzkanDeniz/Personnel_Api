77f020f9e976960a54ae1afbf071d8ee780ddc65
ozkan <ozkandnz09@gmail.com>
2024-12-29 16:54:32 +0300

    swaggerAutogen

--- swaggerAutogen.js
+++ swaggerAutogen.js
@@ -4,7 +4,4 @@ require("dotenv").config;
 const HOST = process.env?.HOST || "127.0.0.1";
 const PORT = process.env?.PORT || 8000;
 
-const swaggerAutogen = require("swagger-autogen")({
-  openapi: "3.0.0 ",
-  language: "tr-TR",
-});
+const swaggerAutogen = require("swagger-autogen")({});
