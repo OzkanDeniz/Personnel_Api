cf576acd3bdca1a244ba15e48ebb679ef45979de
ozkan <ozkandnz09@gmail.com>
2025-01-03 13:29:09 +0300

    swagger personnels

--- src/controllers/personnel.controller.js
+++ src/controllers/personnel.controller.js
@@ -8,6 +8,23 @@ const passwordEncrypt = require("../helpers/passwordEncrypt");
 
 module.exports = {
   list: async (req, res) => {
+    /*
+    
+    #swagger.tags =['Personnels']
+    #swagger.summary = 'List Personnels'
+    #swagger.description = `
+      You can send query with endpoint for filter[], search[], sort[], page and limit.
+            <ul> Examples:
+                <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                <li>URL/?<b>page=2&limit=1</b></li>
+            </ul>
+     `
+    
+    }
+
+    */
     const data = await res.getModelList(Personnel, {} , "departmentId");
     res.status(200).send({
       error: false,
